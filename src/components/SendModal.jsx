dd07896a6aa9167746fedbbdd4ffac04a5ed96a2
Asish <z5430777@ad.unsw.edu.au>
2024-04-24 11:29:43 +1000

    can only send to a valid email

--- src/components/SendModal.jsx
+++ src/components/SendModal.jsx
@@ -10,6 +10,7 @@ export default function SendModal ({ fileIds, setEnableCheck }) {
   const token = '0b93479c-996f-4aa5-b932-a30c5e8fe41d';
   const [open, setOpen] = React.useState(false);
   const [email, setEmail] = React.useState('');
+  const [canSend, setCanSend] = React.useState(false);
   const setModalOpen = () => {
     setOpen(true)
   };
@@ -18,9 +19,16 @@ export default function SendModal ({ fileIds, setEnableCheck }) {
     setEnableCheck(false);
   }
   const handleSetEmail = (newEmail) => {
-    // check for valid email
+    setCanSend(validateEmail(email))
     setEmail(newEmail)
   }
+  const validateEmail = (email) => {
+    return String(email)
+        .toLowerCase()
+        .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+  };
   const handleSubmit = async () => {
     try {
       const response = await axios.post('http://localhost:3001/khats/sendMultiple', 
@@ -51,7 +59,7 @@ export default function SendModal ({ fileIds, setEnableCheck }) {
           <hr color="#000"/>
           <TextField fullWidth id="presentation-name" label="Recipient email" variant="outlined" onChange={ (e) => handleSetEmail(e.target.value)} value={email}/>
           <br/>
-          <Button style={{ marginTop: '10px' }}fullWidth variant="contained" onClick={handleSubmit}>Send</Button>
+          <Button style={{ marginTop: '10px' }}fullWidth disabled={!canSend} variant="contained" onClick={handleSubmit}>Send</Button>
           <Button style={{ marginTop: '10px' }}fullWidth variant="contained" onClick={close}>Cancel</Button>
         </Box>
       </Modal>
